73e13b83058809e3bc849f94e3be5359bb32568c
Mathieu <leers.mathieu@gmail.com>
2020-09-30 16:00:46 +0200

    init connection to scrap

--- Collect_data/Function.py
+++ Collect_data/Function.py
@@ -9,6 +9,7 @@ import time
 import pandas as pd
 import os
 
+
 def init_webdriver(is_firefox: bool = False) -> webdriver:
     if is_firefox:
         profile = webdriver.FirefoxProfile()
@@ -21,3 +22,24 @@ def init_webdriver(is_firefox: bool = False) -> webdriver:
         options.add_experimental_option("prefs", {"profile.managed_default_content_settings.images": 2})
         driver = webdriver.Chrome(options=options)
     return driver
+
+
+def init_connection(driver: webdriver, url, title: str = None, check_button: bool = False):
+    driver.get(url)
+    if title is not None:
+        assert title in driver.title
+    if check_button:
+        # take away the popup (only once)
+        try:
+            element = WebDriverWait(driver, 10).until(
+                EC.presence_of_element_located((By.ID, 'uc-btn-accept-banner'))
+            )
+            if element is not None:
+                element.click()
+                print("button clicked")
+        except Exception as e:
+            print(e)
+
+
+def check_number_of_pages(driver):
+    driver.find_elemen
--- Collect_data/main.py
+++ Collect_data/main.py
@@ -1,11 +1,18 @@
-import Function
-
-is_firefox = False
-
-######################################
-#     Get the urls of each page      #
-######################################
+from Collect_data.Function import *
 
 base_url = "https://www.immoweb.be/"
-
+search_url = "https://www.immoweb.be/fr/recherche/appartement/a-vendre?countries=BE&isALifeAnnuitySale=false&page=1&orderBy=relevance "
 is_firefox = False
+
+driver = init_webdriver()
+
+init_connection(driver, search_url, title="Immoweb", check_button=True)
+time.sleep(2)
+number_of_pages = driver.find_elements_by_css_selector('div.search-results__header a.pagination__link span.button__label')
+number_of_pages = number_of_pages[2]
+number_of_pages = number_of_pages.get_attribute('innerHTML')
+
+print(number_of_pages)
+
+
+driver.close()
